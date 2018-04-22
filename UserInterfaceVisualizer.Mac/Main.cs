6ac12dc71f10ccd076900a1699b9cd3473741bf2
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-04-22 21:04:54 +0530

    initial push

---
+++ UserInterfaceVisualizer.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace UserInterfaceVisualizer.Mac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
