64a612df22dabd042318d50f0071180dd5c0ee86
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-04-25 18:13:20 +0530

    images

--- UserInterfaceVisualizer/MainPage.xaml.cs
+++ UserInterfaceVisualizer/MainPage.xaml.cs
@@ -13,5 +13,23 @@ namespace XamarinFormsStarterKit.UserInterfaceVisualizer
 		{
 			InitializeComponent();
 		}
+
+		protected override void  OnAppearing()
+		{
+			base.OnAppearing();
+
+			iPhoneX.Source = "/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone X.png";
+			iPhone8.Source = "/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone 8.png";
+			iPhone8p.Source ="/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone 8 Plus.png";
+			iPhoneSE.Source = "/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone SE.png";
+
+			android720_1280_5.Source ="/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone X.png";
+			android1080_1920_5_2.Source ="/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone X.png";
+			android1440_2560_5_8.Source= "/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone X.png";
+			android1440_2560_6_2.Source ="/Users/arunbalakrishnan/UserInterfaceTestRunner/UserInterfaceTestRunner/Screenshots/ios/iPhone X.png";
+
+             
+		}
+        
 	}
 }
